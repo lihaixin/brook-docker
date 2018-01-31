7fe3a3e0b6e0c076a7ae0ad4b47f1bd0ce99826a
lihaixin <lihaixin@15099.net>
2018-01-31 22:05:28 +0800

    change the BURST

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,12 @@ fi
 sleep 2
 # $execfile --help
 if [[ ${protocol} == "server" ]]; then
-	# $execfile $protocol --help
-	# $execfile $protocol --listen ${serverlistenip} --password ${password} --tcpDeadline $tcpDeadline $OPTIONS > /dev/sdtout 2 >&1 &
+	$execfile $protocol --help
+        $execfile $protocol --listen ${serverlistenip} --password ${password} --tcpDeadline $tcpDeadline $OPTIONS > /dev/sdtout 2>&1 &
         iptables -F
         iptables -A INPUT -p tcp -m state --state NEW --dport $LIMIT_PORT -m connlimit --connlimit-above $LIMIT_CONN -j DROP
         tc qdisc add dev eth0 root tbf rate $RATE burst $BURST latency $LATENCY
-        #  watch -n $INTERVAL tc -s qdisc ls dev eth0
-        $execfile $protocol --listen ${serverlistenip} --password ${password} --tcpDeadline $tcpDeadline $OPTIONS
+        watch -n $INTERVAL tc -s qdisc ls dev eth0
 elif [[ ${protocol} == "client" ]]; then
 	$execfile $protocol --help
 	$execfile $protocol --listen $clientlistenip --ip $clientip  --server $serverlistenip  --password $password $OPTIONS  
